a234a1dca119f8b8c7836ec273213b73335d8cd7
AleksGirardey <alexis.girardey@gmail.com>
2020-09-03 10:38:29 +0200

    feat: Liane, MovingPlatform, IActivable, Checkpoint

--- Assets/Runtime/GodFatherJam/Scripts/Obstacle/Liane.cs
+++ Assets/Runtime/GodFatherJam/Scripts/Obstacle/Liane.cs
@@ -8,7 +8,7 @@ public class Liane : MonoBehaviour {
         
         LightShieldBehaviour lightShieldBehaviour = other.GetComponent<LightShieldBehaviour>();
 
-        if (!lightShieldBehaviour.IsOverloaded()) return;
+        if (!lightShieldBehaviour.IsOverloaded) return;
             
         objectToActivate?.Activate();
         enabled = false;
